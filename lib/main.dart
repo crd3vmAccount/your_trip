c9d9a2545926ecb804650762a84a4aa83093022b
crd3vm <112511567+crd3vmAccount@users.noreply.github.com>
2024-04-09 23:31:34 -0400

    Simple example of taking a photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:your_trip/views/sign_in_view.dart';
+import 'package:image_picker/image_picker.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -10,14 +12,50 @@ void main() async {
   );
   runApp(
     MaterialApp(
-      title: "Your Trip",
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.red,
-        brightness: Brightness.dark,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const SignInView(),
-    ),
+        title: "Your Trip",
+        theme: ThemeData(
+          useMaterial3: true,
+          primarySwatch: Colors.red,
+          brightness: Brightness.dark,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const Scaffold(
+          body: TakePicture(),
+        )),
   );
+}
+
+class TakePicture extends StatefulWidget {
+  const TakePicture({super.key});
+
+  @override
+  State createState() {
+    return _TakePictureState();
+  }
+}
+
+class _TakePictureState extends State<TakePicture> {
+  File? file;
+  final ImagePicker imagePicker = ImagePicker();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () async {
+            var image = await imagePicker.pickImage(source: ImageSource.camera);
+            file = File(image!.path);
+          },
+          child: const Text('press me'),
+        ),
+        file != null
+            ? Image.file(
+                file!,
+                fit: BoxFit.cover,
+              )
+            : const Text('no image selected')
+      ],
+    );
+  }
 }
