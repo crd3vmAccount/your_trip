70628b190ee8e818040dd5d1e0d6ca747091e0f1
crd3vm <112511567+crd3vmAccount@users.noreply.github.com>
2024-04-16 23:28:24 -0400

    Implemented Gallery Card

---
+++ lib/views/gallery_list/gallery_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class GalleryCard extends StatelessWidget {
+  const GalleryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                  child: Text(
+                    'Title',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 3, 0, 0),
+                  child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    color: Colors.grey, // Placeholder for image
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _buttonColumn(
+              () => {},
+              () => {},
+              () => {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttonColumn(void Function() onMapClick,
+      void Function() onGalleryClick,
+      void Function() onShareClick,) {
+    return Column(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.map_outlined),
+          onPressed: () {
+            // Add your button functionality here
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.auto_awesome_mosaic_rounded),
+          onPressed: () {
+            // Add your button functionality here
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.share),
+          onPressed: () {
+            // Add your button functionality here
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:your_trip/views/gallery_list/gallery_card.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Column(
+        children: [
+          GalleryCard(),
+        ],
+      ),
+    );
   }
 }
