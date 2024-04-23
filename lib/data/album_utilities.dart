851f7581ce37483dd8fba4729c8953dd1db2f311
crd3vm <112511567+crd3vmAccount@users.noreply.github.com>
2024-04-23 02:05:34 -0400

    Refactored album utilities

--- lib/data/album_utilities.dart
+++ lib/data/album_utilities.dart
@@ -1,15 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+void requireAuthentication() {
+  if (FirebaseAuth.instance.currentUser == null) {
+    throw ArgumentError("Account is not authenticated");
+  }
+}
+
+String getUid() {
+  requireAuthentication();
+  var user = FirebaseAuth.instance.currentUser;
+  return user!.uid;
+}
+
 CollectionReference<Map<String, dynamic>> getUserAlbumCollection() {
   var user = FirebaseAuth.instance.currentUser;
-  if (user == null) {
-    throw ArgumentError("Account is not authenticated");
-  } else {
-    var fireStore = FirebaseFirestore.instance;
-    var users = fireStore.collection("users");
-    var userContent = users.doc(user.uid);
-    var userAlbums = userContent.collection("albums");
-    return userAlbums;
-  }
+  var fireStore = FirebaseFirestore.instance;
+  var users = fireStore.collection("users");
+  var userContent = users.doc(user!.uid);
+  var userAlbums = userContent.collection("albums");
+  return userAlbums;
 }
