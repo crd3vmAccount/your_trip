2111a559f9cb145b57793b40da23ba124e8813e3
crd3vm <112511567+crd3vmAccount@users.noreply.github.com>
2024-04-22 23:05:13 -0400

    added toString to Album + made simplified interface

--- lib/data/album.dart
+++ lib/data/album.dart
@@ -1,10 +1,19 @@
 class Album {
   final String displayName;
   late final String queryName;
-  final List<String>? sharedWith;
-  final List<String>? photos;
+  final List<String> sharedWith;
+  final List<String> photos;
 
-  Album(this.displayName, {this.sharedWith, this.photos}) {
+  Album(
+    this.displayName, {
+    this.sharedWith = const [],
+    this.photos = const [],
+  }) {
     queryName = displayName.toLowerCase();
+  }
+
+  @override
+  String toString() {
+    return 'Album{displayName: $displayName, queryName: $queryName, sharedWith: $sharedWith, photos: $photos}';
   }
 }
